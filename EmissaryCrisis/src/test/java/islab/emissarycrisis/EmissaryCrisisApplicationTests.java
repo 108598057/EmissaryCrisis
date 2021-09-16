eb0fa1cc23e91787792a2ba1ae282d68725c59c6
bao <aaa0928279227@gmail.com>
2021-09-16 16:15:06 +0800

    initial

---
+++ EmissaryCrisis/src/test/java/islab/emissarycrisis/EmissaryCrisisApplicationTests.java
@@ -0,0 +1,13 @@
+package islab.emissarycrisis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissaryCrisisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
